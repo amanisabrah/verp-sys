8c8150fd15e1d0b0627d6171e7b0bf34eebfb53e
Your Name <amanisabrah@gmail.com>
2024-10-21 12:18:38 +0300

    import roles file to the home page and cleane home page code

--- verp-system/home-page.test.js
+++ verp-system/home-page.test.js
@@ -1,21 +1,18 @@
-import { Role, Selector } from "testcafe"
+import { Selector } from "testcafe"
+import { adminRole } from "./roles"  // Import the admin role from roles.js
+
+    const username= Selector ("#Login_I")
+    const passwordInput = Selector('#Password_I_CLND')
+    const showpassbutton = Selector('#Password_B0')
+    const rememberCheck = Selector('#RememberMe_S_D')
+    const loginButton = Selector('#Button_CD')
+    const enterpassword = Selector('#Password_I')  
 
 fixture`Open the Home-Page successfylly`
     .page`http://localhost:58307/Dictionaries/Account/Login`
-    //.before(async t =>{test setup goes here, axait runDatabaseReset(), await seedTestData()})
     .beforeEach(async t => {
-        //run befor each test 
-        await t
-        .setTestSpeed(0.8)
+        await t.setTestSpeed(0.8)
     })
-    //.after(async t =>{ cleaning test data, Logging and sending data tp monitoring systems})
-    //.afterEach(async t => { runs after each test    })
-        const username= Selector ("#Login_I")
-        const passwordInput = Selector('#Password_I_CLND')
-        const showpassbutton = Selector('#Password_B0')
-        const rememberCheck = Selector('#RememberMe_S_D')
-        const loginButton = Selector('#Button_CD')
-        const enterpassword = Selector('#Password_I')  
     test('incorrect password', async t =>{
             const incorrectpass= Selector('#Password_EC')
             await t
@@ -37,74 +34,38 @@ fixture`Open the Home-Page successfylly`
             .click(rememberCheck)
             .click(loginButton)
             .expect(incorrectusername.innerText).contains('User Name Not Exist')
-        })
-///////////defina a Role to skip login in each test////////////
-    const adminRole = Role('http://localhost:58307/Dictionaries/Account/Login', async t => {
-        await t
-        .click(username)
-        .typeText(username, 'admin')
-        .click(passwordInput)
-        .typeText(enterpassword, 'Qwe!23-2016')
-        .click(showpassbutton)
-        .click(rememberCheck)
-        .click(loginButton)
-        //navigateTo('http://localhost:58307/Home/CompanySelection')
-        const selectcompany = Selector ('.list-group-item.Company.noselect[data-id="1"]')
-        await t
-        .skipJsErrors()
-        .click(selectcompany)
-        .skipJsErrors(false) 
-        .navigateTo('http://localhost:58307/')
-    }, { preserveUrl: true })
-
+        })        
     test('LogIn successfully', async t => {
-       await t.useRole(adminRole)
-    })
+        await t.useRole(adminRole)
+     })
+ 
 
 //Fixture for interacting with other dashboards
 fixture`Open cards successfully`
     .page`http://localhost:58307/Dictionaries/Account/Login`
     .skipJsErrors()  // Globally ignoring JS errors
     .beforeEach(async t=> {
-        await t
-        .useRole(adminRole)
+        await t.useRole(adminRole)
     })
     .afterEach(async t=> {
         const homeLink = Selector('#NavigationPaths').find('a').withText('Home')
         await t.click(homeLink)
     })
-    test('POS', async t => {
-        await t
-        .navigateTo('http://localhost:58307/Inventory/Dictionary/PointOfSales?action=AddNew')
-    })
-    test('Financial', async t => {
-        await t.navigateTo('http://localhost:58307/Accounting/Accountancy/FinancialDashboardsView')
-    })
-    test('HR', async t =>{
-        await t.navigateTo('http://localhost:58307/HRMS/Dictionary/HumanResourcesManagementDashboardsView')
-    })
-    test('BI Dashboards', async t =>{
-        await t.navigateTo('http://localhost:58307/DSS/Dictionary/DashboardsAndReportsView')
-    })
-   /* test('Time Management Dashboards View', async t => {
-    await t.navigateTo('http://localhost:58307/Attendance/Home/TimeManagementDashboardsView')
-    }) */
-   test('Inventory', async t =>{
-        await t.navigateTo('http://localhost:58307/Inventory/Home/InventoryDashboardView')
-    })
-    test('Work Flow', async t =>{
-        await t.navigateTo('http://localhost:58307/WorkFlow/Dictionary/WorkFlowDiagrams')
-    })
-    test('Configurations', async t =>{
-        await t.navigateTo('http://localhost:58307/Dictionaries/Settings/Settings')
-    })
-    test('Sales', async t =>{
-        await t.navigateTo('http://localhost:58307/Inventory/Home/SalesDashboardView')
-    })
-    test('Purchasing', async t =>{
-        await t.navigateTo('http://localhost:58307/Inventory/Home/PurchaseDashboardView')
-    })
-    test('Approval', async t =>{
-        await t.navigateTo('http://localhost:58307/HRMS/Approval/Approval')
-    })
-
+    const dashboardTests =[
+        {name: 'POS', URL:'http://localhost:58307/Inventory/Dictionary/PointOfSales?action=AddNew'},
+        {name: 'Financial', URL:'http://localhost:58307/Accounting/Accountancy/FinancialDashboardsView'},
+        {name: 'HR', URL:'http://localhost:58307/HRMS/Dictionary/HumanResourcesManagementDashboardsView'},
+        {name: 'BI Dashboards', URL:'http://localhost:58307/DSS/Dictionary/DashboardsAndReportsView'},
+        //{name: 'Time Management Dashboards View', URL:'http://localhost:58307/Attendance/Home/TimeManagementDashboardsView'},
+        {name: 'Inventory', URL:'http://localhost:58307/Inventory/Home/InventoryDashboardView'},
+        {name: 'Work Flow', URL:'http://localhost:58307/WorkFlow/Dictionary/WorkFlowDiagrams'},
+        {name: 'Configurations', URL:'http://localhost:58307/Dictionaries/Settings/Settings'},
+        {name: 'Sales', URL:'http://localhost:58307/Inventory/Home/SalesDashboardView'},
+        {name: 'Purchasing', URL:'http://localhost:58307/Inventory/Home/PurchaseDashboardView'},
+        {name: 'Approval', URL:'http://localhost:58307/HRMS/Approval/Approval'},
+    ]
+    dashboardTests.forEach(dashboard => {
+        test(dashboard.name, async t => {
+            await t.navigateTo(dashboard.URL);
+        });
+    });
