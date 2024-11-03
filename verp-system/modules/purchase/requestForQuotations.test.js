674e4b6e581e636a249757ed35bb0cbe4ce1ea36
Your Name <amanisabrah@gmail.com>
2024-11-03 14:50:46 +0300

    in request for quotation add a new test case to open the new edit form immediately

--- verp-system/modules/purchase/requestForQuotations.test.js
+++ verp-system/modules/purchase/requestForQuotations.test.js
@@ -7,24 +7,37 @@ import { open_Purchaasing_Dashboard } from "../../helpers.test"
 export const RFQ_Test_Cases = () => {
     const pageTitle = Selector('#PageTitle')
     const datesFilterForm = Selector('#DatesFilterForm_0_2')
+    const navigatebar= Selector('#CenterMenu_DXI3_T')
+    const rfqGridview = Selector('#CenterMenu_DXI3i0_T')
+    const newform = Selector ('#CenterMenu_DXI3i0_')
     
     fixture`RFQ Test Cases`
     .page`http://localhost:58307/Dictionaries/Account/Login`
     .skipJsErrors()
     .beforeEach(async t => {
         await t.useRole(adminRole)
+        await open_Purchaasing_Dashboard(t)  // Reuse the helper function to open the Purchase dashboard
     })
 
     test('Open the grid view for RFQ', async t => {
-        await open_Purchaasing_Dashboard(t)  // Reuse the helper function to open the Purchase dashboard
         await t
         .setTestSpeed(0.1)
         await t
-        .hover('#CenterMenu_DXI3_T')
-        .click('#CenterMenu_DXI3i0_T')
+        .hover(navigatebar)
+        .click(rfqGridview)
         .expect(pageTitle.innerText).contains('Request For Quotations')
         .expect(datesFilterForm.visible).ok('Dates Filter Form should be visible')// if the test is stopped should display the message ('Dates Filter Form should be visible')
+        //display the toolbar
+
     })
+
+    test('open new edit form immediately', async t =>{
+        await t
+        .hover(navigatebar)
+        .click(newform).find('[title="New"].menu-button')//should open new edit form for the RFQ
+
+    })
+
 
 
 }
