5ab889a12f29d5041705a94cdc4e6b63e4cf9c85
Your Name <amanisabrah@gmail.com>
2024-12-26 16:21:40 +0300

    add all test cases for login

--- erp-subscription/landing-page/login.js
+++ erp-subscription/landing-page/login.js
@@ -6,6 +6,11 @@ export const Landing_LogIn_Test_Cases = () => {
     .beforeEach(async t => {
         await t.setTestSpeed(0.8)
     })
+    .afterEach(async t => {
+        // Reload the page after each test
+       // await t.navigateTo(`https://register.vtech-help.com/#/landing-pages`);
+    })
+    const code_Value='962'
 
     test('1- Submit failed: ', async t => {
         await t 
@@ -14,10 +19,105 @@ export const Landing_LogIn_Test_Cases = () => {
             .hover(landing_Page_Selectors.error_List)
             .click(landing_Page_Selectors.pause_Icon)
 
-        const errorMassage = await landing_Page_Selectors.error_List_Text.innerText
+        const errorMassage = await landing_Page_Selectors.all_IsRequired.innerText
       
-        console.log('1-Submit failed success. Error Messages: ', errorMassage)
+        console.log('1-Submit failed success, error Messages: ', errorMassage)
     })
 
+    test ('2- Verify if a user will be able to login with a valid username and valid phone number.', async t => {
+        const code_Value='962'
+        await t
+            .click(landing_Page_Selectors.user_Name)
+            .typeText(landing_Page_Selectors.user_Name, 'Amani Sabra')
+            .click(landing_Page_Selectors.phone_Key)
+            .expect(landing_Page_Selectors.list_Of_Keys.visible).ok('The list should be displayed')
+            .typeText(landing_Page_Selectors.search_bar, code_Value)
+            .wait(1000)
+            .pressKey('tab')
+            .pressKey('enter')
+            //.expect(landing_Page_Selectors.list_Of_Keys.visible).notOk('The list should be hidden after selecting the entered code.')
+            .click(landing_Page_Selectors.phone_Number)
+            .typeText(landing_Page_Selectors.phone_Number, '789552270') // should be decimal max 9 numbers
+            .click(landing_Page_Selectors.submit)
+            .expect(landing_Page_Selectors.error_List.visible).ok('Error List should displayed')
+            .hover(landing_Page_Selectors.error_List)
+            .click(landing_Page_Selectors.pause_Icon)
+            const errorMassage = await landing_Page_Selectors.email_IsRequired.innerText
+      
+        console.log('2- Verify if a user will be able to login with a valid username and valid phone number:', errorMassage)
+            
+    })
+    test ('3- Verify if a user will be able to login with a valid email and valid phone number.', async t => {
+        const code_Value='962'
+        await t
+            .click(landing_Page_Selectors.phone_Key)
+            .expect(landing_Page_Selectors.list_Of_Keys.visible).ok('The list should be displayed')
+            .typeText(landing_Page_Selectors.search_bar, code_Value)
+            .wait(1000)
+            .pressKey('tab')
+            .pressKey('enter')
+            //.expect(landing_Page_Selectors.list_Of_Keys.visible).notOk('The list should be hidden after selecting the entered code.')
+            .click(landing_Page_Selectors.phone_Number)
+            .typeText(landing_Page_Selectors.phone_Number, '789552270') // should be decimal max 9 numbers
+            .click(landing_Page_Selectors.email)
+            .typeText(landing_Page_Selectors.email, 'amani@gmail.com')
+            .click(landing_Page_Selectors.submit)
+            .expect(landing_Page_Selectors.error_List.visible).ok('Error List should displayed')
+            .hover(landing_Page_Selectors.error_List)
+            .click(landing_Page_Selectors.pause_Icon)
+            const errorMassage = await landing_Page_Selectors.name_IsRequired.innerText
+
+        console.log('3-Verify if a user will be able to login with a valid email and valid phone number:', errorMassage)
+            
+    })
+
+    test ('4- Verify if a user will be able to login with an invalid email.', async t => {
+        await t
+            .click(landing_Page_Selectors.user_Name)
+            .typeText(landing_Page_Selectors.user_Name, 'Amani Sabra')
+            .click(landing_Page_Selectors.phone_Key)
+            .expect(landing_Page_Selectors.list_Of_Keys.visible).ok('The list should be displayed')
+            .typeText(landing_Page_Selectors.search_bar, code_Value)
+            .wait(1000)
+            .pressKey('tab')
+            .pressKey('enter')
+            //.expect(landing_Page_Selectors.list_Of_Keys.visible).notOk('The list should be hidden after selecting the entered code.')
+            .click(landing_Page_Selectors.phone_Number)
+            .typeText(landing_Page_Selectors.phone_Number, '789552270') // should be decimal max 9 numbers
+            .click(landing_Page_Selectors.email)
+            .typeText(landing_Page_Selectors.email, 'test.io.com')
+            .click(landing_Page_Selectors.submit)
+            .expect(landing_Page_Selectors.error_List.visible).ok('Error List should displayed')
+            .hover(landing_Page_Selectors.error_List)
+            .click(landing_Page_Selectors.pause_Icon)
+            const errorMassage = await landing_Page_Selectors.invalid_Email.innerText
+
+        console.log('3-Verify if a user will be able to login with a valid email and valid phone number:', errorMassage)
+        console.log('numbers or letters and these two symbols "@" and "." in this order: letter or num(@)letter or num(.)letter or num')    
+    })
+    test('5- Login successfully.', async t => {
+        await t
+            .click(landing_Page_Selectors.user_Name)
+            .typeText(landing_Page_Selectors.user_Name, 'Amani Sabra')
+            .click(landing_Page_Selectors.phone_Key)
+            .expect(landing_Page_Selectors.list_Of_Keys.visible).ok('The list should be displayed')
+            .typeText(landing_Page_Selectors.search_bar, code_Value)
+            .wait(1000)
+            .pressKey('tab')
+            .pressKey('enter')
+            .click(landing_Page_Selectors.phone_Number)
+            .typeText(landing_Page_Selectors.phone_Number, '789552270') // should be decimal max 9 numbers
+            .click(landing_Page_Selectors.email)
+            .typeText(landing_Page_Selectors.email, 'amani@vtech-sys.com')
+            .click(landing_Page_Selectors.submit)
+            //.navigateTo('https://register.vtech-help.com/#/landing-page?CLIID=459')
+            .expect(landing_Page_Selectors.success_List.visible).ok('success message should displayed')
+            .hover(landing_Page_Selectors.success_List)
+            .click(landing_Page_Selectors.pause_Icon)
+        
+            const successMassage = await landing_Page_Selectors.success_Message.innerText
+        console.log('5-Login Successfully:', successMassage)
+
+    })
 
 }
--- erp-subscription/sub-common/sub-helpers/sub-selectors.js
+++ erp-subscription/sub-common/sub-helpers/sub-selectors.js
@@ -1,13 +1,21 @@
 import { Selector } from "testcafe";
 
 export const landing_Page_Selectors = {
-    user_Name : Selector('#dx_dx-25477e08-74ae-8258-ce3d-d20e4de3bc17_name'),
+    landing_Page: Selector('div').withText('Landing Page'),
+    user_Name: Selector('input[placeholder="Name"]'), // Using placeholder for stability
     phone_Key : Selector ('.dx-lookup-field'),
-    phone_Number : Selector ('.dx-texteditor-input').nth(1),
-    email : Selector ('#dx_dx-25477e08-74ae-8258-ce3d-d20e4de3bc17_email'),
+    phone_Number : Selector ('.dx-texteditor-input').nth(2),
+    email : Selector ('input[placeholder="Email"]'),
     submit : Selector('div').withText('SUBMIT').nth(7),
     error_List : Selector ('.dx-overlay-content.dx-toast-content.dx-toast-error'),
+    success_List: Selector('.dx-overlay-content.dx-toast-content.dx-toast-success'),
     pause_Icon: Selector('.dx-icon.dx-icon-handlevertical').nth(1),
-    error_List_Text: Selector('.dx-overlay-content.dx-toast-content.dx-toast-error').withText('Name Is Required').withText('Email Is Required')
+    all_IsRequired: Selector('.dx-overlay-content.dx-toast-content.dx-toast-error').withText('Name Is Required').withText('Email Is Required'),
+    email_IsRequired: Selector('.dx-overlay-content.dx-toast-content.dx-toast-error').withText('Email Is Required'),
+    name_IsRequired: Selector('.dx-overlay-content.dx-toast-content.dx-toast-error').withText('Name Is Required'),
+    invalid_Email: Selector('.dx-overlay-content.dx-toast-content.dx-toast-error').withText('Invalid Email'),
+    success_Message: Selector('.dx-overlay-content.dx-toast-content.dx-toast-success').withText('Success!'),
+    list_Of_Keys: Selector('.dx-lookup-search-wrapper'),
+    search_bar: Selector('input[placeholder="Search"]'),
 
 }
